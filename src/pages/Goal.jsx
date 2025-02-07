ed2a663649c76c37f66caccae6688578bcbdb127
Aleks <windows02@internet.ru>
2025-02-07 23:08:20 +0400

    header done

---
+++ src/pages/Goal.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+function Goal(){
+    return (
+        <Header />
+    )
+}
+
+export default Goal;
---
+++ src/pages/SystemExplanation.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+function Explanation(){
+    return (
+        <Header />
+    )
+}
+
+export default Explanation;
